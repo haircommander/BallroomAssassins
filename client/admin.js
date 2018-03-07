6f899e655899fd4659cb05e8998144a816bfaf15
peterjh <peterjh@umich.edu>
2018-03-07 18:46:18 -0500

    preparing to port to not profile AGAIN

--- client/admin.js
+++ client/admin.js
@@ -3,21 +3,16 @@ Template.admin.events({
     'click #leggo-button': function(e, t) {
         e.preventDefault();
         console.log("coming soon to a theater near you.");
-/*
-        Meteor.logout(function(error) {
-            if (error) {
-                return swal({
-                    title: "There was an error",
-                    text: "Sorry! There was an error logging out, try again!",
-                    timer: 1700,
-                    showConfirmButton: false,
-                    type: "error"
-                });
-            } else {
-                FlowRouter.go('/login');
-            }
+        Meteor.call('admin.shuffleTargets',
+          {}
+        , (err, res) => {
+          if (err) {
+            alert(err);
+          } else {
+            // success!
+          }
         });
-  */
+        console.log("shuffled!");
         //return false;
     }
 });
---
+++ server/admin.js
@@ -0,0 +1,63 @@
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+}
+
+const shuffleTargets = {
+  name: 'admin.shuffleTargets',
+  // Factor out validation so that it can be run independently (1)
+  validate() {
+   if (!Roles.userIsInRole(Meteor.user(), [ 'admin' ])) {
+    throw new Meteor.Error("admin.wrong", "You're not allowed to do this");
+   };
+  },
+  // Factor out Method body so that it can be called independently (3)
+  run({}) {
+    let users = Meteor.users.find({}).fetch();
+    shuffle(users);
+    console.log(users);
+    let target = users[users.length - 1];
+    users.forEach(user => {
+        user.profile.targetId = target._id;
+        user.profile.targetName = target.profile.fullName;
+        
+        Meteor.users.update({_id: user._id}, {
+            $set: { profile: user.profile}
+        });
+        target = user;
+    });
+  },
+  // Call Method by referencing the JS object (4)
+  // Also, this lets us specify Meteor.apply options once in
+  // the Method implementation, rather than requiring the caller
+  // to specify it at the call site.
+  call(args, callback) {
+    const options = {
+      returnStubValue: true,     // (5)
+      throwStubExceptions: true  // (6)
+    }
+    Meteor.apply(this.name, [args], options, callback);
+  }
+};
+// Actually register the method with Meteor's DDP system
+Meteor.methods({
+  [shuffleTargets.name]: function (args) {
+    shuffleTargets.validate.call(this, args);
+    shuffleTargets.run.call(this, args);
+  }
+});
+
+
+shuffleTargets
