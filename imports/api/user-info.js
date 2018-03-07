e73ef1779ef6dee86f3a3b65645a45a4b2543f3e
peterjh <peterjh@umich.edu>
2018-03-07 13:19:28 -0500

    saving work

---
+++ imports/api/user-info.js
@@ -0,0 +1,44 @@
+import { Mongo } from 'meteor/mongo';
+export const UserInfo = new Mongo.Collection('user-info'); 
+export const createUser = {
+  name: 'user-info.createUser',
+  // Factor out validation so that it can be run independently (1)
+  validate(args) {
+    new SimpleSchema({
+      fullName: {type: String},
+      agentName: {type: String},
+      id: {type: String}
+    }).validate(args)
+  },
+  // Factor out Method body so that it can be called independently (3)
+  run({ id, fullName, agentName}) {
+    UserInfo.insert({
+        _id: id,
+        fullName: fullName, 
+        agentName: agentName, 
+        kills: 0,
+        killCode: Random.id(8),
+        target: "" 
+    });
+  },
+  // Call Method by referencing the JS object (4)
+  // Also, this lets us specify Meteor.apply options once in
+  // the Method implementation, rather than requiring the caller
+  // to specify it at the call site.
+  call(args, callback) {
+    const options = {
+      returnStubValue: true,     // (5)
+      throwStubExceptions: true  // (6)
+    }
+    Meteor.apply(this.name, [args], options, callback);
+  }
+};
+// Actually register the method with Meteor's DDP system
+Meteor.methods({
+  [createUser.name]: function (args) {
+    createUser.validate.call(this, args);
+    createUser.run.call(this, args);
+  }
+});
+
+
--- server/user-info.js
+++ server/user-info.js
@@ -1,6 +1,3 @@
-const UserInfo = new Mongo.Collection('user-info');
-
-
 Meteor.publish('thisUser', function() {
     var currentUser = this.userId;
     if (currentUser) {
@@ -17,45 +14,3 @@ Meteor.publish('thisUser', function() {
         return this.ready();
     }
 });
-
-export const createUser = {
-  name: 'user-info.createUser',
-  // Factor out validation so that it can be run independently (1)
-  validate(args) {
-    new SimpleSchema({
-      fullName: {type: String},
-      agentName: {type: String},
-      id: {type: String}
-    }).validate(args)
-  },
-  // Factor out Method body so that it can be called independently (3)
-  run({ id, fullName, agentName}) {
-    UserInfo.insert({
-        _id: id,
-        fullName: fullName, 
-        agentName: agentName, 
-        kills: 0,
-        killCode: Random.id(8),
-        target: "" 
-    });
-  },
-  // Call Method by referencing the JS object (4)
-  // Also, this lets us specify Meteor.apply options once in
-  // the Method implementation, rather than requiring the caller
-  // to specify it at the call site.
-  call(args, callback) {
-    const options = {
-      returnStubValue: true,     // (5)
-      throwStubExceptions: true  // (6)
-    }
-    Meteor.apply(this.name, [args], options, callback);
-  }
-};
-// Actually register the method with Meteor's DDP system
-Meteor.methods({
-  [createUser.name]: function (args) {
-    createUser.validate.call(this, args);
-    createUser.run.call(this, args);
-  }
-});
-
