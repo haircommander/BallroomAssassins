6d07dabed2ce1b28c7b0b20012f2ae4c35972cbe
peterjh <peterjh@umich.edu>
2018-03-08 00:39:22 -0500

    admin fixing

--- client/admin.js
+++ client/admin.js
@@ -14,5 +14,23 @@ Template.admin.events({
         });
         console.log("shuffled!");
         //return false;
+    },
+    'click #logout-button': function(e, t) {
+        e.preventDefault();
+        Meteor.logout(function(error) {
+            if (error) {
+                return swal({
+                    title: "There was an error",
+                    text: "Sorry! There was an error logging out, try again!",
+                    timer: 1700,
+                    showConfirmButton: false,
+                    type: "error"
+                });
+            } else {
+                FlowRouter.go('/login');
+            }
+        });
+        return false;
     }
+
 });
